a2b1b0b183d0d3a24f74a1d57525d890d0ba49c7
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-13 14:14:35 +0800

    Add basic styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
+import Logo from "./components/logo";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Logo />
+      <div>
+        <h2>Buttons</h2>
+      <Button class="btn btn__primary" text="Primary" />
+      <Button class="btn btn__secondary" text="Secondary" />
+      <Button class="btn btn__tertiary" text="Tertiary" />
+      </div>
     </div>
   );
 }
---
+++ src/components/logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo: React.FC = function () {
+  return (
+    <span className="logo">
+      <span className="logo__front">Find</span>
+      <span className="logo__back">NUS</span>
+    </span>
+  );
+};
+
+export default Logo;
