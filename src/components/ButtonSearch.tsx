1e9e19e4dcf0b382bbb40d72fd71f8cb6451fef1
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-15 00:19:00 +0800

    Add home page styling

--- src/components/ButtonSearch.tsx
+++ src/components/ButtonSearch.tsx
@@ -3,7 +3,7 @@ import Search from "@mui/icons-material/Search";
 
 const ButtonSearch: React.FC = function () {
   return (
-    <div className="btn btn--search">
+    <div className="btn btn--icon btn--search">
       <Search style={{ color: "white", fontSize: 32 }} />
       <span>Search</span>
     </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Logo from "../components/logo";
+import Button from "../components/Button";
+import SearchBar from "../components/SearchBar";
+import ButtonFound from "../components/ButtonFound";
 
 const Home: React.FC = function () {
-  return <div>Hello world</div>;
+  return (
+    <div className="home">
+      <div className="home__nav">
+        <Logo />
+        <Button class="btn btn--tertiary" text="Log In" />
+      </div>
+      <div className="body">
+        <h1 className="home__header">Lost something? Start here.</h1>
+        <SearchBar prompt="Search for an item" />
+        <ButtonFound />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
