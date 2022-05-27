cdb71e214ddee602f25a1f46b2ca181f6d79913d
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-28 01:15:02 +0800

    Add search results styling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -16,7 +16,6 @@ const Home: React.FC = function () {
         <SearchBar prompt="Search for an item" />
         <ButtonFound />
       </section>
-      <section className="search-results"></section>
     </div>
   );
 };
