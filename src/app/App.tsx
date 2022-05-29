54a630eac87d48f22b6fe7e268d4ebd22f87c1b1
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-05-29 21:03:17 +0800

    Add protected route demo

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./App.scss";
 import Home from "../pages/Home";
 import ComponentsView from "../pages/ComponentsView";
 import Login from "../pages/Login";
 import SearchPage from "../pages/SearchPage";
+import { useAppSelector } from "../hooks";
+import { selectAuthIsLoggedIn } from "../features/auth/authSlice";
+import Dashboard from "../pages/Dashboard";
 
 function App() {
+  const isLoggedIn = useAppSelector(selectAuthIsLoggedIn);
+
   useEffect(() => {
     document.title = "FindNUS";
   }, []);
@@ -15,9 +20,11 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        {!isLoggedIn && <Route path="/login" element={<Login />} />}
+        {isLoggedIn && <Route path="/dashboard" element={<Dashboard />} />}
         <Route path="/search" element={<SearchPage />} />
         <Route path="/components" element={<ComponentsView />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
