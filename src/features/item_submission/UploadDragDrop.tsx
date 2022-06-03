eee06a4782de88bdf40775c05f743a2c7ad37238
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-03 17:11:42 +0800

    Add image preview on upload

--- src/features/item_submission/UploadDragDrop.tsx
+++ src/features/item_submission/UploadDragDrop.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { DRAG_DROP_MESSAGE, IMGUR_IMAGE_FORMATS } from "../../constants";
 import ImageIcon from "@mui/icons-material/Image";
@@ -11,11 +11,17 @@ const UploadDragDrop: React.FC<UploadDragDropProps> = function (
   props: UploadDragDropProps
 ) {
   const [file, setFile] = useState<File>();
+  // set to true as workaround for initial onDraggingStateChange execution
+  const [isHover, setIsHover] = useState(true);
+  const [fileURL, setFileURL] = useState("");
   const handleChange = (file: File) => {
     setFile(file);
   };
-  
-  console.log(file);
+
+  // Generate file URL to display uploaded image
+  useEffect(() => {
+    file && setFileURL(URL.createObjectURL(file));
+  }, [file]);
 
   return (
     <FileUploader
@@ -23,11 +29,24 @@ const UploadDragDrop: React.FC<UploadDragDropProps> = function (
       name="item-submission"
       types={IMGUR_IMAGE_FORMATS}
       classes={`file-upload ${props.className}`}
+      hoverTitle="Drop image here"
+      onDraggingStateChange={() => setIsHover((prev) => !prev)}
     >
-      <span className="file-upload__icon">
-        <ImageIcon fontSize="inherit" />
-      </span>
-      {DRAG_DROP_MESSAGE}
+      <div className="file-upload__contents">
+        {fileURL && (
+          <img
+            src={fileURL}
+            className="file-upload__image"
+            alt="Uploaded image"
+          />
+        )}
+        {!isHover && !file && (
+          <span className="file-upload__icon">
+            <ImageIcon fontSize="inherit" />
+          </span>
+        )}
+        {!isHover && !file && DRAG_DROP_MESSAGE}
+      </div>
     </FileUploader>
   );
 };
