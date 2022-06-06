ad0c1eb736bc4797dba9f16fb01d2b5216af87a7
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-07 01:31:38 +0800

    Add basic styling for dashboard

--- src/pages/dashboard/DashboardNav.tsx
+++ src/pages/dashboard/DashboardNav.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import {
   ROUTE_DASHBOARD_ITEMS,
   ROUTE_DASHBOARD_MANAGE,
@@ -7,20 +7,43 @@ import {
 } from "../../constants";
 
 const DashboardNav: React.FC = function () {
+  const location = useLocation();
+  const [currentPath, setCurrentPath] = useState(ROUTE_DASHBOARD_PROFILE);
+
+  useEffect(() => {
+    const { pathname } = location;
+    if (pathname.match(ROUTE_DASHBOARD_PROFILE))
+      return setCurrentPath("profile");
+    if (pathname.match(ROUTE_DASHBOARD_ITEMS)) return setCurrentPath("items");
+    if (pathname.match(ROUTE_DASHBOARD_MANAGE)) return setCurrentPath("manage");
+  }, [location]);
+
   return (
     <section className="dashboard-nav">
       <ul className="dashboard-nav__list">
-        <li className="dashboard-nav__item">
+        <li
+          className={`dashboard-nav__item ${
+            currentPath === "profile" ? "dashboard-nav__item--current" : ""
+          }`}
+        >
           <Link to={ROUTE_DASHBOARD_PROFILE} className="link--wrapper">
             Profile
           </Link>
         </li>
-        <li className="dashboard-nav__item">
+        <li
+          className={`dashboard-nav__item ${
+            currentPath === "items" ? "dashboard-nav__item--current" : ""
+          }`}
+        >
           <Link to={ROUTE_DASHBOARD_ITEMS} className="link--wrapper">
             Items
           </Link>
         </li>
-        <li className="dashboard-nav__item">
+        <li
+          className={`dashboard-nav__item ${
+            currentPath === "manage" ? "dashboard-nav__item--current" : ""
+          }`}
+        >
           <Link to={ROUTE_DASHBOARD_MANAGE} className="link--wrapper">
             Manage
           </Link>
