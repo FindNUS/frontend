bb4c8c32af50a6941b520225263a089097d428b5
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-11 03:59:40 +0800

    Add found item POST on submit logic

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,1 +1,2 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const ENDPOINT_ITEM_FOUND = `${API_BASE_URL}/item/new/found`;
--- src/features/item_submission/ItemSubmissionForm.tsx
+++ src/features/item_submission/ItemSubmissionForm.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonSubmit from "../../components/buttons/ButtonSubmit";
 import FormField from "../../components/form/FormField";
+import PopupMessage from "../../components/PopupMessage";
+import { ENDPOINT_ITEM_FOUND } from "../../constants";
 import { useAppDispatch } from "../../hooks";
+import useAxiosPost from "../../hooks/useAxiosPost";
 import {
   setSubmitDate,
   setSubmitDescription,
@@ -12,8 +15,85 @@ import {
 } from "./submitItemSlice";
 import UploadDragDrop from "./UploadDragDrop";
 
+interface SchemaSubmitFoundItem {
+  Name: string;
+  Date: string;
+  Location: string;
+  Category: string;
+  Contact_method: string;
+  Contact_details: string;
+  Item_details: string;
+  Image_base64: string;
+}
+
+const processFoundItemForAPI = (data: {
+  name: string;
+  date: Date;
+  location: string;
+  category: string;
+  contactMethod: string;
+  contactDetails: string;
+  itemDetails: string;
+  base64URL: string;
+}) => {
+  return {
+    Name: data.name,
+    Date: data.date.toISOString(),
+    Location: data.location,
+    Category: data.category,
+    Contact_method: data.contactMethod,
+    Contact_details: data.contactDetails,
+    Item_details: data.itemDetails,
+    Image_base64: data.base64URL,
+  };
+};
+
+const headers = {
+  "Content-Type": "application/json",
+};
+
+const DUMMY_PAYLOAD = {
+  name: "Water Bottle",
+  date: new Date("2019-08-24T14:15:22Z"),
+  location: "E4A DSA Lab",
+  category: "Cards",
+  contactMethod: "Telegram",
+  contactDetails: "FindNUS",
+  itemDetails: "Blue, with a sticker and broken handle",
+  base64URL: "string",
+};
+
 const ItemSubmissionForm: React.FC = function () {
   const dispatch = useAppDispatch();
+  const [postData, setPostData] = useState<SchemaSubmitFoundItem>({
+    Name: "",
+    Date: "",
+    Location: "",
+    Category: "",
+    Contact_method: "",
+    Contact_details: "",
+    Item_details: "",
+    Image_base64: "",
+  });
+
+  const [result, postToAPI] = useAxiosPost({
+    url: ENDPOINT_ITEM_FOUND,
+    headers: JSON.stringify(headers),
+    payload: JSON.stringify(postData),
+  });
+
+  const {
+    error: submitError,
+    isLoading: isSubmitting,
+    data: submitResponse,
+  } = result;
+
+  const handleSubmitForm = (ev: React.FormEvent) => {
+    ev.preventDefault();
+    setPostData(() => processFoundItemForAPI(DUMMY_PAYLOAD));
+    console.log(postData);
+    postToAPI();
+  };
 
   const handleDescriptionChange = (ev: React.FormEvent) => {
     const { value } = ev.target as HTMLInputElement;
@@ -35,13 +115,24 @@ const ItemSubmissionForm: React.FC = function () {
     const { value } = ev.target as HTMLInputElement;
     dispatch(setSubmitContactDetails(value));
   };
-  const handleImageURLChange = (url: string) => {
+  const handleImageURLChange = async (url: string) => {
     dispatch(setSubmitImageURL(url));
   };
 
   return (
-    <form className="submit-item__form">
+    <form className="submit-item__form" onSubmit={handleSubmitForm}>
       <div className="submit-item__form--fields">
+        {isSubmitting && (
+          <PopupMessage status="loading" message="Submitting item..." />
+        )}
+        {submitError && !isSubmitting && !submitResponse && (
+          <PopupMessage
+            status="error"
+            message={`
+              Item submission failed: ${submitError.message}
+            `}
+          />
+        )}
         <FormField
           onChange={handleDescriptionChange}
           labelContent="Item Description"
@@ -70,7 +161,9 @@ const ItemSubmissionForm: React.FC = function () {
           type="textarea"
           disabled={false}
         />
-        <ButtonSubmit className="btn btn--primary" text="Submit" />
+        {!isSubmitting && (
+          <ButtonSubmit className="btn btn--primary" text="Submit" />
+        )}
       </div>
       <UploadDragDrop
         className="submit-item__form--upload"
