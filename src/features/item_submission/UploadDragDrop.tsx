345df34431973a2380263113f42179401a19673e
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-17 16:35:34 +0800

    Convert image to base64 on upload

--- src/features/item_submission/UploadDragDrop.tsx
+++ src/features/item_submission/UploadDragDrop.tsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { FileUploader } from "react-drag-drop-files";
 import { DRAG_DROP_MESSAGE, IMGUR_IMAGE_FORMATS } from "../../constants";
 import ImageIcon from "@mui/icons-material/Image";
+import useConvertFileToBase64 from "../../hooks/useConvertFileToBase64";
+import { useAppDispatch } from "../../hooks";
+import { setSubmitImageState } from "./submitItemSlice";
 
 interface UploadDragDropProps {
   className: string;
@@ -11,6 +14,7 @@ interface UploadDragDropProps {
 const UploadDragDrop: React.FC<UploadDragDropProps> = function (
   props: UploadDragDropProps
 ) {
+  const dispatch = useAppDispatch();
   const [file, setFile] = useState<File>();
   // set to true as workaround for initial onDraggingStateChange execution
   const [isHover, setIsHover] = useState(true);
@@ -19,16 +23,40 @@ const UploadDragDrop: React.FC<UploadDragDropProps> = function (
     setFile(file);
   };
   const { className, onImageUpload } = props;
+  const [convertResult, convertLoading, convertError, convertCallback] =
+    useConvertFileToBase64(file);
 
   // Generate file URL to display uploaded image
   useEffect(() => {
     if (!file) return;
     setFileURL(URL.createObjectURL(file));
+    convertCallback(file);
   }, [file]);
 
   useEffect(() => {
     onImageUpload(fileURL);
   }, [fileURL]);
+
+  // update image conversion state
+  useEffect(() => {
+    dispatch(
+      setSubmitImageState({
+        type: "RESULT",
+        data: convertResult,
+        isLoading: convertLoading,
+      })
+    );
+  }, [convertResult]);
+
+  useEffect(() => {
+    dispatch(
+      setSubmitImageState({
+        type: "ERROR",
+        data: convertError,
+        isLoading: convertLoading,
+      })
+    );
+  }, [convertError]);
 
   return (
     <FileUploader
