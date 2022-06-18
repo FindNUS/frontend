ee1ebaa4df187b1ebd5be6725057b56d6ecdf9b3
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-18 18:24:14 +0800

    Update dropdown menu options

--- src/components/form/DropdownButton.tsx
+++ src/components/form/DropdownButton.tsx
@@ -1,25 +1,29 @@
 import React from "react";
 import { nanoid } from "nanoid";
+import { DropdownOption } from "../../constants";
 
 interface DropdownButtonProps {
   dropdownName: string;
   dropdownID: string;
-  options: string[];
+  options: DropdownOption[];
+  onChange: (ev: React.FormEvent) => void;
 }
 
 const DropdownButton: React.FC<DropdownButtonProps> = function (
   props: DropdownButtonProps
 ) {
-  const { options, dropdownName, dropdownID } = props;
+  const { options, dropdownName, dropdownID, onChange } = props;
   return (
-    <select name={dropdownName} id={dropdownID} className="dropdown">
+    <select
+      name={dropdownName}
+      id={dropdownID}
+      className="dropdown"
+      onChange={onChange}
+    >
       {options.map((item) => {
         return (
-          <option
-            value={item.toLowerCase().split(" ").join("-")}
-            key={nanoid()}
-          >
-            {item}
+          <option value={item.key} key={nanoid()}>
+            {item.value}
           </option>
         );
       })}
--- src/constants/submit_item.ts
+++ src/constants/submit_item.ts
@@ -7,3 +7,27 @@ export const IMGUR_IMAGE_FORMATS = [
   "TIFF",
 ];
 export const DRAG_DROP_MESSAGE = `Click to upload or drag and drop your image`;
+
+export interface DropdownOption {
+  key: string;
+  value: string;
+}
+
+export const DROPDOWN_DEFAULT_KEY = "Select";
+export const SUBMIT_FOUND_CATEGORIES: DropdownOption[] = [
+  { value: "Select Category", key: DROPDOWN_DEFAULT_KEY },
+  { value: "Etc", key: "Etc" },
+  { value: "Cards", key: "Cards" },
+  { value: "Notes", key: "Notes" },
+  { value: "Electronics", key: "Electronics" },
+  { value: "Bottles", key: "Bottles" },
+];
+export const SUBMIT_FOUND_CONTACT_METHODS: DropdownOption[] = [
+  { value: "Select Contact Method", key: DROPDOWN_DEFAULT_KEY },
+  { value: "NUS Security", key: "nus_security" },
+  { value: "Telegram", key: "telegram" },
+  { value: "WhatsApp", key: "whatsapp" },
+  { value: "WeChat", key: "wechat" },
+  { value: "Line", key: "line" },
+  { value: "Phone Number", key: "phone_number" },
+];
