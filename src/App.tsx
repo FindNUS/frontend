34c340552638041c016a95f2d93954535977b3ee
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-18 21:03:51 +0800

    Add routing for viewing search results

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import ComponentsView from "./pages/ComponentsView";
 import Login from "./pages/Login";
 import SearchPage from "./pages/SearchPage";
+import ViewSearchResult from "./features/search/ViewSearchResult";
 import { useAppSelector } from "./hooks";
 import { selectAuthIsLoggedIn } from "./features/auth/authSlice";
 import Dashboard from "./pages/Dashboard";
@@ -14,6 +15,7 @@ import {
   ROUTE_HOME,
   ROUTE_LOGIN,
   ROUTE_SEARCH,
+  ROUTE_SEARCH_INNER_VIEW_ITEM,
   ROUTE_SUBMIT_ITEM,
   ROUTE_SUBMIT_ITEM_FORM,
   ROUTE_SUBMIT_ITEM_INNER_FORM,
@@ -22,6 +24,7 @@ import {
 import SubmitItemPage from "./pages/SubmitItemPage";
 import ItemSubmissionPost from "./features/item_submission/ItemSubmissionPost";
 import ItemSubmissionForm from "./features/item_submission/ItemSubmissionForm";
+import SearchContainer from "./pages/SearchContainer";
 
 function App() {
   const isLoggedIn = useAppSelector(selectAuthIsLoggedIn);
@@ -36,7 +39,13 @@ function App() {
         <Route path={ROUTE_HOME} element={<Home />} />
         {!isLoggedIn && <Route path={ROUTE_LOGIN} element={<Login />} />}
         {isLoggedIn && <Route path={ROUTE_DASHBOARD} element={<Dashboard />} />}
-        <Route path={ROUTE_SEARCH} element={<SearchPage />} />
+        <Route path={ROUTE_SEARCH} element={<SearchPage />}>
+          <Route path={ROUTE_SEARCH} element={<SearchContainer />} />
+          <Route
+            path={ROUTE_SEARCH_INNER_VIEW_ITEM}
+            element={<ViewSearchResult />}
+          />
+        </Route>
         <Route path={ROUTE_COMPONENTS} element={<ComponentsView />} />
         <Route path={ROUTE_SUBMIT_ITEM} element={<SubmitItemPage />}>
           <Route
