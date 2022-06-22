91d6f394db7108cfd0885eda6cf945b575e223e5
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-22 14:31:21 +0800

    Add error message display for form fields

--- src/components/form/DropdownButton.tsx
+++ src/components/form/DropdownButton.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { nanoid } from "nanoid";
 import { DropdownOption } from "../../constants";
+import PopupMessage from "../PopupMessage";
 
 interface DropdownButtonProps {
   dropdownName: string;
@@ -8,28 +9,34 @@ interface DropdownButtonProps {
   options: DropdownOption[];
   onChange: (ev: React.FormEvent) => void;
   selected: string;
+  isInvalid?: { status: boolean; error: string };
 }
 
 const DropdownButton: React.FC<DropdownButtonProps> = function (
   props: DropdownButtonProps
 ) {
-  const { options, dropdownName, dropdownID, onChange } = props;
+  const { options, dropdownName, dropdownID, onChange, isInvalid } = props;
   return (
-    <select
-      name={dropdownName}
-      id={dropdownID}
-      className="dropdown"
-      onChange={onChange}
-      value={props.selected}
-    >
-      {options.map((item) => {
-        return (
-          <option value={item.key} key={nanoid()}>
-            {item.value}
-          </option>
-        );
-      })}
-    </select>
+    <div className="dropdown-container">
+      {isInvalid?.status && (
+        <PopupMessage status="error" message={isInvalid.error} />
+      )}
+      <select
+        name={dropdownName}
+        id={dropdownID}
+        className="dropdown"
+        onChange={onChange}
+        value={props.selected}
+      >
+        {options.map((item) => {
+          return (
+            <option value={item.key} key={nanoid()}>
+              {item.value}
+            </option>
+          );
+        })}
+      </select>
+    </div>
   );
 };
 
