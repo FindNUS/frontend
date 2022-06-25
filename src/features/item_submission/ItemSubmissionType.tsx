64e666014d0d6e9be7dda33e97f62ddb67d12d31
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-25 19:18:11 +0800

    Add button routing for submit item type selection

--- src/features/item_submission/ItemSubmissionType.tsx
+++ src/features/item_submission/ItemSubmissionType.tsx
@@ -1,11 +1,37 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import BigButton from "../../components/buttons/BigButton";
+import {
+  QUERY_SUBMIT_TYPE_KEY,
+  QUERY_SUBMIT_TYPE_VALUE_FOUND,
+  QUERY_SUBMIT_TYPE_VALUE_LOST,
+  ROUTE_SUBMIT_ITEM_FORM,
+} from "../../constants";
 
 const ItemSubmissionType: React.FC = function () {
+  const navigate = useNavigate();
+
+  const handleClickFound = () =>
+    navigate(
+      `${ROUTE_SUBMIT_ITEM_FORM}?${QUERY_SUBMIT_TYPE_KEY}=${QUERY_SUBMIT_TYPE_VALUE_FOUND}`
+    );
+  const handleClickLost = () =>
+    navigate(
+      `${ROUTE_SUBMIT_ITEM_FORM}?${QUERY_SUBMIT_TYPE_KEY}=${QUERY_SUBMIT_TYPE_VALUE_LOST}`
+    );
+
   return (
     <div className="submit-item__type">
-      <BigButton colour="tertiary" text="Found item" />
-      <BigButton colour="secondary" text="Lost item" />
+      <BigButton
+        colour="tertiary"
+        text="Found item"
+        onClick={handleClickFound}
+      />
+      <BigButton
+        colour="secondary"
+        text="Lost item"
+        onClick={handleClickLost}
+      />
     </div>
   );
 };
