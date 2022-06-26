7e4d8f9976640b566624a672a34188e8757791fe
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-06-26 17:17:34 +0800

    Add reset button for item filter

--- src/features/preview_items/PreviewFilter.tsx
+++ src/features/preview_items/PreviewFilter.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import Button from "../../components/buttons/Button";
 import DropdownButton from "../../components/form/DropdownButton";
 import {
   DROPDOWN_DEFAULT_KEY,
@@ -19,10 +20,17 @@ const PreviewFilter: React.FC = function () {
     navigate(`${location.pathname}?${QUERY_VIEW_ITEM_CATEGORY}=${value}`);
   };
 
+  const handleResetFilter = () => {
+    setSelectedCategory(DROPDOWN_DEFAULT_KEY);
+    navigate(
+      `${location.pathname}?${QUERY_VIEW_ITEM_CATEGORY}=${DROPDOWN_DEFAULT_KEY}`
+    );
+  };
+
   return (
     <section className="search-filter">
       <h4>Filter Items</h4>
-      <form>
+      <form className="search-filter__form">
         <DropdownButton
           dropdownName="filter-category"
           dropdownID="filter-category"
@@ -30,6 +38,13 @@ const PreviewFilter: React.FC = function () {
           onChange={handleCategoryChange}
           selected={selectedCategory}
         />
+        {selectedCategory !== DROPDOWN_DEFAULT_KEY && (
+          <Button
+            class="btn btn--secondary"
+            text="Reset filters"
+            onClick={handleResetFilter}
+          />
+        )}
       </form>
     </section>
   );
