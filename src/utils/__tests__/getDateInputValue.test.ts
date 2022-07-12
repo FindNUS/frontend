d35cafce5b830997d9865767c2a8eae73dae4a46
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-07-12 23:18:46 +0800

    Fix timezone-biased testing

--- src/utils/__tests__/getDateInputValue.test.ts
+++ src/utils/__tests__/getDateInputValue.test.ts
@@ -1,11 +1,52 @@
 import getDateInputValue from "../getDateInputValue";
 
-const testDates = [
-  { input: "2022-12-13T00:00:00.000Z", expected: "2022-12-13" },
-  { input: "2022-12-12T15:59:59.999Z", expected: "2022-12-12" }, // adjust for Singapore Time
-  { input: "2022-12-12T16:00:00.000Z", expected: "2022-12-13" }, // adjust for Singapore Time
-  { input: "2022-12-12T16:00:00.001Z", expected: "2022-12-13" },
-];
+const testDates = (() => {
+  const timezoneOffset = new Date().getTimezoneOffset() * 60 * 1000;
+
+  return [
+    {
+      input: "2022-12-12T23:59:59.999Z",
+      expected: new Date(
+        new Date("2022-12-12T23:59:59.999Z").valueOf() - timezoneOffset
+      )
+        .toJSON()
+        .slice(0, 10),
+    },
+    {
+      input: "2022-12-13T00:00:00.000Z",
+      expected: new Date(
+        new Date("2022-12-13T00:00:00.000Z").valueOf() - timezoneOffset
+      )
+        .toJSON()
+        .slice(0, 10),
+    },
+    {
+      input: "2022-12-13T00:00:00.001Z",
+      expected: new Date(
+        new Date("2022-12-13T00:00:00.000Z").valueOf() - timezoneOffset
+      )
+        .toJSON()
+        .slice(0, 10),
+    },
+    // test cases for Singapore time zone
+    {
+      input: "2022-12-12T15:59:59.999Z",
+      expected: new Date(
+        new Date("2022-12-12T15:59:59.999Z").valueOf() - timezoneOffset
+      )
+        .toJSON()
+        .slice(0, 10),
+    },
+    {
+      input: "2022-12-12T16:00:00.001Z",
+      expected: new Date(
+        new Date("2022-12-12T16:00:00.001Z").valueOf() - timezoneOffset
+      )
+        .toJSON()
+        .slice(0, 10),
+    },
+  ];
+})();
 
 describe("Input element date helper function", () => {
   it("returns date in the correct format", () => {
