f7b449d49776cbf15e9c3d9a6e6253d68f9ff049
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2022-07-19 12:58:35 +0800

    Add responsive degisn for home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = function () {
     <div className="home background background--main">
       <Header isHomePage={true} isLoggedIn={isLoggedIn} />
       <section className="home__body">
-        <h1 className="home__header">Lost something? Start here.</h1>
+        <h1 className="home__header">Lost&nbsp;something? Start&nbsp;here.</h1>
         <SearchBar prompt={SEARCH_BAR_PROMPT} />
       </section>
       <div className="home__message text-white-shadow">
