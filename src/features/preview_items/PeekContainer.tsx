59ef9ce5bd6231cbe44fa799731f2a29c7a5c3c1
Jin Xuan Ow Yong <owyongjinxuan@gmail.com>
2022-09-30 16:15:57 +0800

    Fixed issue with loading next page
    PreviewFilter will not be unloaded when loading new filter parameters

--- src/features/preview_items/PeekContainer.tsx
+++ src/features/preview_items/PeekContainer.tsx
@@ -1,15 +1,11 @@
 import React from "react";
-import { useAppSelector } from "../../hooks";
 import PreviewFilter from "./PreviewFilter";
 import PreviewItems from "./PreviewItems";
-import { selectPreviewLoading } from "./previewItemsSlice";
 
 const PeekContainer: React.FC = function () {
-  const loading = useAppSelector(selectPreviewLoading);
-
   return (
     <div className="search-container">
-      {!loading && <PreviewFilter isPeek={true} />}
+      <PreviewFilter isPeek={true} />
       <PreviewItems isPeek={true} />
     </div>
   );
